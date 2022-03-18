5a4f02979aec0e33f2e115f51fd72539160d9b8e
LetsPoke <59581396+LetsPoke@users.noreply.github.com>
2022-03-18 10:51:15 +0100

    reformatting with prettier

--- hms-app/src/components/IdeaCardSmall.tsx
+++ hms-app/src/components/IdeaCardSmall.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import favIcon from "../images/favIcon.png"
+import React from 'react'
+import favIcon from '../images/favIcon.png'
 
-function IdeaCardSmall(idea: any){
-    return(
-        <>
-            <h3>{idea.titel}</h3>
-            <p>{idea.description}</p>
-            <div style={{display: "flex", gap: "5px"}}>
-                <img src={favIcon} style={{width: "30px", height: "30px"}}/>
-                <p>number of favs: {idea.favNumber}</p>
-            </div>
-        </>
-    )
+function IdeaCardSmall(idea: any) {
+  return (
+    <>
+      <h3>{idea.titel}</h3>
+      <p>{idea.description}</p>
+      <div style={{ display: 'flex', gap: '5px' }}>
+        <img src={favIcon} style={{ width: '30px', height: '30px' }} />
+        <p>number of favs: {idea.favNumber}</p>
+      </div>
+    </>
+  )
 }
 
 export default IdeaCardSmall
--- hms-app/src/pages/YourIdea.tsx
+++ hms-app/src/pages/YourIdea.tsx
@@ -1,27 +1,23 @@
-import React from "react";
-import ideaData from "../TestIdeaData";
-import IdeaCardSmall from "../components/IdeaCardSmall";
+import React from 'react'
+import ideaData from '../TestIdeaData'
+import IdeaCardSmall from '../components/IdeaCardSmall'
 
-function YourIdea(){
-
-    const ideas = ideaData.map(idea => {
-        return (
-            <IdeaCardSmall
-                {...idea} //spreads the item in its components in 1 line of code
-            />
-        )
-    })
-
-
-    return(
-        <>
-            <h1>this is the your idea page</h1>
-            <h2>Your Ideas:</h2>
-            <div className="idea-list">
-                {ideas}
-            </div>
-        </>
+function YourIdea() {
+  const ideas = ideaData.map((idea) => {
+    return (
+      <IdeaCardSmall
+        {...idea} //spreads the item in its components in 1 line of code
+      />
     )
+  })
+
+  return (
+    <>
+      <h1>this is the your idea page</h1>
+      <h2>Your Ideas:</h2>
+      <div className="idea-list">{ideas}</div>
+    </>
+  )
 }
 
 export default YourIdea
