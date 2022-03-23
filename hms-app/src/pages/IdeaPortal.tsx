567f07c81c7ed60cd383f792b946721d4da6b1f2
Jakob Panten <jakob@panten-sh.de>
2022-03-23 13:53:54 +0100

    add mantine grid

--- hms-app/src/pages/IdeaPortal.tsx
+++ hms-app/src/pages/IdeaPortal.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import IdeaCardSmall from '../components/IdeaCardSmall'
 import ideaData from '../test/TestIdeaData'
 import CategoryDropdown from '../components/CategoryDropdown'
+import { Grid } from '@mantine/core'
 
 function IdeaPortal() {
   const IdeasList = ideaData.map((idea) => {
@@ -26,7 +27,13 @@ function IdeaPortal() {
         </div>
       </div>
       <h3>chosen categorie is: </h3>
-      <div className="idea-list">{IdeasList}</div>
+      <div className="idea-list">
+        <Grid>
+          <Grid.Col span={4}>1</Grid.Col>
+          <Grid.Col span={4}>2</Grid.Col>
+          <Grid.Col span={4}>3</Grid.Col>
+        </Grid>
+      </div>
     </>
   )
 }
