e6a0a98a0e4a2626638bffbcf6155676a38bb932
Jakob Panten <jakob@panten-sh.de>
2022-03-23 22:37:39 +0100

    add dark / lightmode switch in navbar

--- hms-app/src/pages/Layout.tsx
+++ hms-app/src/pages/Layout.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import { AppShell, Container } from '@mantine/core'
+import {
+  AppShell,
+  Container,
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from '@mantine/core'
 import HeaderMenu from '../components/HeaderMenu'
+import { useColorScheme } from '@mantine/hooks'
 
 const menuLinks = [
   { link: 'home', label: 'Home' },
@@ -26,23 +33,36 @@ const menuLinks = [
 ]
 
 const Layout = () => {
+  const preferredColorScheme = useColorScheme()
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme)
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
   return (
-    <AppShell
-      header={<HeaderMenu links={menuLinks} />}
-      styles={(theme) => ({
-        main: {
-          backgroundColor:
-            theme.colorScheme === 'dark'
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-          minHeight: 'calc(100vh - 56px)',
-        },
-      })}
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      <Container size={'xl'}>
-        <Outlet />
-      </Container>
-    </AppShell>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles>
+        <AppShell
+          header={<HeaderMenu links={menuLinks} />}
+          styles={(theme) => ({
+            main: {
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+              minHeight: 'calc(100vh - 56px)',
+            },
+          })}
+        >
+          <Container size={'xl'}>
+            <Outlet />
+          </Container>
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
 
