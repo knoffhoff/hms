b1ef0b871f2104f922c8bd4ce99eb80d699b584f
LetsPoke <59581396+LetsPoke@users.noreply.github.com>
2022-03-31 11:37:41 +0200

    removed border

--- hms-app/src/pages/YourIdeas.tsx
+++ hms-app/src/pages/YourIdeas.tsx
@@ -1,4 +1,4 @@
-import { Accordion, Button, Grid, Text } from '@mantine/core'
+import { Accordion, Button } from '@mantine/core'
 import ideaData from '../test/TestIdeaData'
 import { Idea } from '../common/types'
 import IdeaCardList from '../components/IdeaCardList'
@@ -13,6 +13,7 @@ function YourIdeas() {
       <h1>this is the your idea page</h1>
       <Accordion mb={30} icon={false} iconPosition="right">
         <Accordion.Item
+          style={{ border: 'none' }}
           label={
             <Button radius="md" size="md">
               Create new idea
