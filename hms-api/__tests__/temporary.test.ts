80e170b34fa046427073490d77a8089e4f993749
verzano <mitchell.v.berg@gmail.com>
2022-04-06 16:20:12 +0200

    removing test stuff

--- hms-api/__tests__/temporary.test.ts
+++ hms-api/__tests__/temporary.test.ts
@@ -1,9 +1,5 @@
 'use strict';
 
-import {hackathonIds} from '../src/mock/hackathon';
-
 test('A useless example test just to have how tests work for now.', () => {
-  console.log(JSON.stringify(hackathonIds));
-
   expect(2 * 9).toBe(18);
 });
