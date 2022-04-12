a3effcad90966fca2f4c3a0fab41856931b5e7dd
verzano <mitchell.v.berg@gmail.com>
2022-04-12 12:11:33 +0200

    223 reverting changes

--- hms-api/src/handlers/category/get.ts
+++ hms-api/src/handlers/category/get.ts
@@ -1,27 +1,21 @@
 import {buildResponse} from '../../rest/responses';
 import {getCategory} from '../../repository/category-repository';
-import {getHackathon} from '../../repository/hackathon-repository';
-import Category from '../../repository/domain/Category';
 import CategoryResponse from '../../rest/CategoryResponse';
-import Hackathon from '../../repository/domain/Hackathon';
+import {getHackathon} from '../../repository/hackathon-repository';
 import HackathonPreviewResponse from '../../rest/HackathonPreviewResponse';
 
 // eslint-disable-next-line require-jsdoc
 export async function get(event, context, callback) {
   const category = await getCategory(event.pathParameters.id);
-  if (category instanceof Category) {
+  if (category) {
     const hackathon = await getHackathon(category.hackathonId);
-    if (hackathon instanceof Hackathon) {
-      const responseBody = new CategoryResponse(
-          category.id,
-          category.title,
-          category.description,
-          HackathonPreviewResponse.from(hackathon),
-      );
-      callback(null, buildResponse(200, responseBody));
-    } else {
-      callback(null, buildResponse(404, {}));
-    }
+    const responseBody = new CategoryResponse(
+        category.id,
+        category.title,
+        category.description,
+        HackathonPreviewResponse.from(hackathon),
+    );
+    callback(null, buildResponse(200, responseBody));
   } else {
     callback(null, buildResponse(404, {}));
   }
