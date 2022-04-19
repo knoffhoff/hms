43513cd271284afe8cf3f7c5c7615bfbff41a424
MarcelK <59581396+LetsPoke@users.noreply.github.com>
2022-04-19 17:58:50 +0200

    textsearch with loading issues

--- hms-app/src/pages/IdeaPortal.tsx
+++ hms-app/src/pages/IdeaPortal.tsx
@@ -1,17 +1,67 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ideaData from '../test/TestIdeaData'
 import { Input, Group, Title } from '@mantine/core'
 import { Search } from 'tabler-icons-react'
 import IdeaCardList from '../components/IdeaCardList'
+import HackathonDetails from '../components/HackathonDetails'
+import { Hackathon, IdeaPreview } from '../common/types'
+import { getHackathonDetails } from '../actions/GetBackendData'
 
 function IdeaPortal() {
   const [searchTerm, setSearchTerm] = useState('')
+  const hackathonID = '4eb2d486-c786-431e-a4fd-4c093ed30642'
+  const [hackathonData, setHackathonData] = useState({
+    errorHackathonData: false,
+    isLoadingHackathonData: true,
+    title: 'string',
+    startDate: '',
+    endDate: '',
+    participants: null,
+    categories: null,
+    ideas: [] as IdeaPreview[],
+  })
+  const {
+    errorHackathonData,
+    isLoadingHackathonData,
+    title,
+    startDate,
+    endDate,
+    participants,
+    categories,
+    ideas,
+  }: Hackathon = hackathonData
+  const loadSelectedHackathon = () => {
+    getHackathonDetails(hackathonID).then(
+      (data) => {
+        setHackathonData({
+          title: data.title,
+          startDate: data.startDate,
+          endDate: data.endDate,
+          participants: data.participants,
+          categories: data.categories,
+          ideas: data.ideas,
+          errorHackathonData: false,
+          isLoadingHackathonData: false,
+        })
+      },
+      () => {
+        setHackathonData({
+          ...hackathonData,
+          errorHackathonData: true,
+          isLoadingHackathonData: false,
+        })
+      }
+    )
+  }
+  useEffect(() => {
+    loadSelectedHackathon()
+  }, [])
 
   const handleChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value)
   }
 
-  const filteredIdeas = ideaData.filter((item) => {
+  const filteredIdeas = hackathonData.ideas.filter((item) => {
     return item.title.includes(searchTerm)
   })
 
@@ -26,12 +76,17 @@ function IdeaPortal() {
           onChange={handleChangeSearch}
         />
       </Group>
-      <div className="idea-list">
+
+      <div style={{ border: '1px solid red' }}>
         <IdeaCardList
           ideas={filteredIdeas}
           columnSize={6}
           type={'idea-portal'}
         />
+      </div>
+
+      <div style={{ border: '1px solid red' }}>
+        <HackathonDetails hackathonID={hackathonID} />
       </div>
     </>
   )
