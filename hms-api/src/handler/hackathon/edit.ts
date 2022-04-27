c9d74a207570bcbca788b85dc18ed92984bd610d
Mitchell Verzano Berg <mitchell.v.berg@gmail.com>
2022-04-27 12:58:04 +0200

    280 add edit hackathon lambda (#289)
    * 280 added code path for editing hackathons
    
    * 280 added tests
    
    * 278 asserts startDate < endDate (#279)
    
    * 278 asserts startDate < endDate and fixes issues with mockUuid and mockDate
    
    * 278 fixing jest config
    
    * 280 fixed after rebase tests
    
    * 280 adding http client testing call

---
+++ hms-api/src/handler/hackathon/edit.ts
@@ -0,0 +1,17 @@
+import {wrapHandler} from '../handler-wrapper';
+import {buildResponse} from '../../rest/responses';
+import {editHackathon} from '../../service/hackathon-service';
+import HackathonEditRequest from '../../rest/HackathonEditRequest';
+import HackathonEditResponse from '../../rest/HackathonEditResponse';
+
+// eslint-disable-next-line require-jsdoc
+export async function edit(event, context, callback) {
+  await wrapHandler(async () => {
+    const id = event.pathParameters.id;
+    const request = HackathonEditRequest.parse(event.body);
+
+    await editHackathon(id, request.title, request.startDate, request.endDate);
+
+    callback(null, buildResponse(200, new HackathonEditResponse(id)));
+  }, callback);
+}
