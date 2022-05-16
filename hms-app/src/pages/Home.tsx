6410b1c77399d877b6bc5b0499386db7d1ee1674
MarcelK <59581396+LetsPoke@users.noreply.github.com>
2022-05-16 17:21:22 +0200

    identify next hackathon

--- hms-app/src/pages/Home.tsx
+++ hms-app/src/pages/Home.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { HackathonPreview } from '../common/types'
+import { getListOfHackathons } from '../actions/HackathonActions'
 
 function Home() {
+  const [hackathonList, setHackathonList] = useState([] as HackathonPreview[])
+  const [isLoading, setIsLoading] = useState(true)
+  const today = new Date()
+
+  const loadHackathons = () => {
+    getListOfHackathons().then((data) => {
+      setHackathonList(data.hackathons)
+      setIsLoading(false)
+    })
+  }
+
+  const getNextHackathon = hackathonList.filter((hackathon) => {
+    return new Date(hackathon.startDate) > today
+  })
+
+  const timeTillNextHackathonHIHI = !isLoading
+    ? new Date(getNextHackathon[0].startDate).getTime() - today.getTime()
+    : today.getTime()
+
+  useEffect(() => {
+    loadHackathons()
+  }, [])
+
   return (
     <>
       <h1>this is the Startpage</h1>
@@ -16,6 +41,26 @@ function Home() {
         rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
         ipsum dolor sit amet.
       </p>
+
+      {!isLoading && (
+        <div>
+          <h1>
+            Time till next Hackathon{' '}
+            {Math.round(timeTillNextHackathonHIHI / (1000 * 3600 * 24))} days
+            and {Math.round(timeTillNextHackathonHIHI / (1000 * 60 * 60)) % 24}{' '}
+            hours
+          </h1>
+
+          <h2>{getNextHackathon[0].title}</h2>
+          <h2>
+            Start Date: {new Date(getNextHackathon[0].startDate).toDateString()}
+          </h2>
+          <h2>
+            {' '}
+            End Date: {new Date(getNextHackathon[0].endDate).toDateString()}
+          </h2>
+        </div>
+      )}
     </>
   )
 }
