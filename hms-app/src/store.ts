d63b82c7ca8ce5b628b68fedbcf1d0662a37a79a
MarcelK <59581396+LetsPoke@users.noreply.github.com>
2022-05-25 15:01:28 +0200

    added missing export

--- hms-app/src/store.ts
+++ hms-app/src/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counter/counterSlice'
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     counter: counterReducer,
     /*posts: postsReducer,
