e5e27284c28820228c00993a219c2260292b631a
MarcelK <59581396+LetsPoke@users.noreply.github.com>
2022-06-01 13:28:55 +0200

    changed text into mantine text

--- hms-app/src/pages/Home.tsx
+++ hms-app/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { HackathonPreview } from '../common/types'
-import { createStyles, Stepper } from '@mantine/core'
+import { createStyles, Stepper, Text, Title } from '@mantine/core'
 
 const useStyles = createStyles((theme) => ({
   container: {
@@ -134,11 +134,13 @@ function Home() {
 
   return (
     <>
-      <h1>Welcome to the Hack-week Management System</h1>
+      <Title order={1}>Welcome to the Hack-week Management System</Title>
       {!!localStorage.getItem('nextHackathon') && (
         <div>
-          <h2 style={{ textAlign: 'center' }}>Next Hackathon in</h2>
-          <h2 style={{ textAlign: 'center' }}>
+          <Title order={2} style={{ textAlign: 'center' }}>
+            Next Hackathon in
+          </Title>
+          <Title order={2} style={{ textAlign: 'center' }}>
             {
               (
                 (timeTillNextHackathonStart() - today.getTime()) /
@@ -150,36 +152,36 @@ function Home() {
             days and{' '}
             {Math.round(timeTillNextHackathonStart() / (1000 * 3600)) % 24}{' '}
             hours
-          </h2>
-          <div>Next Hackathon: {nextHackathon.title}</div>
-          <div>
+          </Title>
+          <Text>Next Hackathon: {nextHackathon.title}</Text>
+          <Text>
             Start Date: {new Date(nextHackathon.startDate).toLocaleDateString()}
-          </div>
-          <div>
+          </Text>
+          <Text>
             End Date: {new Date(nextHackathon.endDate).toLocaleDateString()}
-          </div>
+          </Text>
           {timelineStepper}
         </div>
       )}
 
       <div className={classes.container}>
         <div>
-          <h3>What is a Hack-week?</h3>
-          <p>
+          <Title order={3}>What is a Hack-week?</Title>
+          <Text>
             In our case, a Hack-week is more or less self - explaining ;) we
             will have a 5-Day long Hack-week that starts on Monday with Idea
             Pitches and ends on Friday with some cool project presentations and
             a winner celebration
-          </p>
+          </Text>
         </div>
         <div>
-          <h3>Why should I Participate?</h3>
-          <p>
+          <Title order={3}>Why should I Participate?</Title>
+          <Text>
             Maybe you have a great idea that you have wanted to work on for
             years? Use the chance to build a prototype! Maybe you want to learn
             or test a new framework? Then this is your save space! You have a
             solution for an existing problem? Come and find allies for it.
-          </p>
+          </Text>
         </div>
       </div>
 
@@ -190,24 +192,24 @@ function Home() {
         className={classes.container}
       >
         <div>
-          <h3>How to use this site?</h3>
-          <p>
+          <Title order={3}>How to use this site?</Title>
+          <Text>
             In the HMS you will be able to participate in a Hackathon, submit
             ideas, see all other ideas and vote for the best idea in the end.
-          </p>
+          </Text>
         </div>
         <div>
-          <h3>How to participate?</h3>
-          <p>
+          <Title order={3}>How to participate?</Title>
+          <Text>
             If you want to participate in a Hackathon, navigate to the Idea
             Portal page, select a Hackathon and click on the participate button
-          </p>
+          </Text>
           ---add screenshot from header here--- ---add screenshot from dropdown
           here--- ---add screenshot from participate button here---
         </div>
         <div>
-          <h3>How to find Ideas?</h3>
-          <p>
+          <Title order={3}>How to find Ideas?</Title>
+          <Text>
             In the Idea Portal, you can select all upcoming Hackathons to see a
             list of submitted ideas. Also, you have the opportunity to search
             for specific idea titles or to only display your favorite ideas. The
@@ -217,12 +219,12 @@ function Home() {
             participate or just save it to decide later you will find a
             participate and favorite button on the bottom of every expanded idea
             card.
-          </p>
+          </Text>
           ---add screenshot of a expanded idea card---
         </div>
         <div>
-          <h3>How to submit Ideas?</h3>
-          <p>
+          <Title order={3}>How to submit Ideas?</Title>
+          <Text>
             If you are already registered for a hackathon and want to submit
             your own ideas, navigate to the "Your Ideas" page. In the "Your
             Ideas" page, you can select the Hackathon you want to submit a new
@@ -232,20 +234,22 @@ function Home() {
             already submitted idea, load the hackathon to see your ideas, then
             expand the Idea Card you want and click on the Edit or Delete button
             on the bottom.
-          </p>
+          </Text>
           ---add a screenshot from the "create idea button" and the edit/delete
           buttons---{' '}
         </div>
         <div>
-          <h3>How to find old Hackathons?</h3>
-          <p>
+          <Title order={3}>How to find old Hackathons?</Title>
+          <Text>
             If you are interested in Past Hackathons or want to find an old idea
             that you remember, you can use the Archive. In the Archive you will
             find a selection of all past hackathons with their submitted ideas.
-          </p>
+          </Text>
         </div>
         <div>
-          <h3>How the Voting and the Hackathon itself will work?</h3>
+          <Title order={3}>
+            How the Voting and the Hackathon itself will work?
+          </Title>
           ---add explanation about the voting system--- ---add space for
           specific explanation?---
         </div>
