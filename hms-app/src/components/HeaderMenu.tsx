8c214281e5cbe724745c4aa55d6bc5e9deb5da1e
MKay <59581396+LetsPoke@users.noreply.github.com>
2022-07-05 16:52:01 +0200

    436 add highlight color to the header menu (#452)
    * removed localstorage
    
    * Update IdeaPortal.tsx
    
    * Update hackathon-seed.json
    
    * added variable for null_date
    
    * Update HeaderMenu.tsx
    
    * Update HeaderMenu.tsx
    
    * added highlight color for header
    
    * update
    
    * update

--- hms-app/src/components/HeaderMenu.tsx
+++ hms-app/src/components/HeaderMenu.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Header, Menu, Group, Burger, Container, Avatar } from '@mantine/core'
 import { useBooleanToggle } from '@mantine/hooks'
 import { SwitchToggle } from './ThemeSwitchToggle'
 import { styles } from '../common/styles'
+import { HEADER_ACTIVE_COLOR } from '../common/constants'
 
 interface HeaderSearchProps {
   links: {
@@ -18,10 +19,21 @@ export default function HeaderMenu({ links }: HeaderSearchProps) {
   const { classes } = styles()
   const today = new Date()
   today.setHours(0, 0, 0, 0)
+  const location = useLocation()
 
   const fullscreenMenu = links.map((link) => {
     return (
-      <Link key={link.label} to={link.link} className={classes.link}>
+      <Link
+        key={link.label}
+        to={link.link}
+        className={classes.link}
+        style={{
+          backgroundColor:
+            location.pathname.slice(1) === link.link
+              ? HEADER_ACTIVE_COLOR
+              : undefined,
+        }}
+      >
         {link.label}
       </Link>
     )
