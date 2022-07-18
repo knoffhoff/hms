de66d4d97610ea606b2424bc091903ece7258498
Jakob Panten <jakob@panten-sh.de>
2022-07-18 12:02:36 +0200

    export login styles from styles file

--- hms-app/src/pages/Login.tsx
+++ hms-app/src/pages/Login.tsx
@@ -11,48 +11,10 @@ import { useMsal } from '@azure/msal-react'
 import idealoIcon from '../assets/idealo-icon.png'
 import { SwitchToggle } from '../components/ThemeSwitchToggle'
 import React from 'react'
-
-const useStyles = createStyles((theme) => ({
-  wrapper: {
-    minHeight: 900,
-    height: '100vh',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundImage:
-      theme.colorScheme === 'dark'
-        ? 'url(https://images.unsplash.com/photo-1475274047050-1d0c0975c63e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80)'
-        : 'url(https://images.unsplash.com/photo-1597200381847-30ec200eeb9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80)',
-  },
-
-  form: {
-    borderRight: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
-    minHeight: 900,
-    maxWidth: 450,
-    paddingTop: 80,
-
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      maxWidth: '100%',
-    },
-  },
-
-  title: {
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-  },
-
-  logo: {
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    width: 120,
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
-}))
+import { loginStyles } from '../common/styles'
 
 const Login = () => {
-  const { classes } = useStyles()
+  const { classes } = loginStyles()
   const { instance } = useMsal()
 
   const login = async () => {
