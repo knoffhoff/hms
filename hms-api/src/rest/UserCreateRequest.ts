f3373926b50973ad6b12ba626027167c6f764de1
Jakob Panten <jakob@panten-sh.de>
2022-07-26 16:03:58 +0200

    add admin role if user is admin

--- hms-api/src/rest/UserCreateRequest.ts
+++ hms-api/src/rest/UserCreateRequest.ts
@@ -1,28 +1,25 @@
 /* eslint-disable require-jsdoc */
 
-import Uuid from '../util/Uuid';
-import Role, {mapStringToRoles} from '../repository/domain/Role';
+import Uuid from "../util/Uuid";
+import Role, { mapStringToRoles } from "../repository/domain/Role";
 
 class UserCreateRequest {
   lastName?: string;
   firstName: string;
   emailAddress: string;
-  roles: Role[];
   skills: Uuid[];
   imageUrl?: string;
 
   constructor(
-      lastName: string,
-      firstName: string,
-      emailAddress: string,
-      roles: Role[],
-      skills: Uuid[],
-      imageUrl: string,
+    lastName: string,
+    firstName: string,
+    emailAddress: string,
+    skills: Uuid[],
+    imageUrl: string
   ) {
     this.lastName = lastName;
     this.firstName = firstName;
     this.emailAddress = emailAddress;
-    this.roles = roles;
     this.skills = skills;
     this.imageUrl = imageUrl;
   }
@@ -30,12 +27,11 @@ class UserCreateRequest {
   static parse(body: string): UserCreateRequest {
     const json = JSON.parse(body);
     return new UserCreateRequest(
-        json.lastName,
-        json.firstName,
-        json.emailAddress,
-        mapStringToRoles(json.roles),
-        json.skills,
-        json.imageUrl,
+      json.lastName,
+      json.firstName,
+      json.emailAddress,
+      json.skills,
+      json.imageUrl
     );
   }
 }
