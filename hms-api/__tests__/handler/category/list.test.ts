20dcbe3df395c8dd0fc431f2da67bff544d4aa87
Jakob Panten <jakob@panten-sh.de>
2022-07-27 12:10:19 +0200

    changed path param on tests

--- hms-api/__tests__/handler/category/list.test.ts
+++ hms-api/__tests__/handler/category/list.test.ts
@@ -1,24 +1,25 @@
-import {randomCategory} from '../../repository/domain/category-maker';
-import {list} from '../../../src/handler/category/list';
-import Uuid, {uuid} from '../../../src/util/Uuid';
-import NotFoundError from '../../../src/error/NotFoundError';
-import CategoryListResponse from '../../../src/rest/CategoryListResponse';
-import * as categoryService from '../../../src/service/category-service';
+import { randomCategory } from "../../repository/domain/category-maker";
+import { list } from "../../../src/handler/category/list";
+import Uuid, { uuid } from "../../../src/util/Uuid";
+import NotFoundError from "../../../src/error/NotFoundError";
+import CategoryListResponse from "../../../src/rest/CategoryListResponse";
+import * as categoryService from "../../../src/service/category-service";
 
 const mockGetCategoryListResponse = jest.fn();
-jest.spyOn(categoryService, 'getCategoryListResponse')
-    .mockImplementation(mockGetCategoryListResponse);
+jest
+  .spyOn(categoryService, "getCategoryListResponse")
+  .mockImplementation(mockGetCategoryListResponse);
 
-describe('List Categorys', () => {
-  test('Happy Path', async () => {
+describe("List Categorys", () => {
+  test("Happy Path", async () => {
     const hackathonId = uuid();
     const category1 = randomCategory();
     const category2 = randomCategory();
     const category3 = randomCategory();
     const category4 = randomCategory();
     const expected = CategoryListResponse.from(
-        [category1, category2, category3, category4],
-        hackathonId,
+      [category1, category2, category3, category4],
+      hackathonId
     );
 
     mockGetCategoryListResponse.mockResolvedValue(expected);
@@ -30,16 +31,16 @@ describe('List Categorys', () => {
     expect(callback).toHaveBeenCalledWith(null, {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'content-type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "content-type": "application/json",
       },
       body: JSON.stringify(expected),
     });
   });
 
-  test('Throws NotFoundError', async () => {
-    const errorMessage = 'reference error message';
+  test("Throws NotFoundError", async () => {
+    const errorMessage = "reference error message";
     mockGetCategoryListResponse.mockImplementation(() => {
       throw new NotFoundError(errorMessage);
     });
@@ -49,16 +50,16 @@ describe('List Categorys', () => {
     expect(callback).toHaveBeenCalledWith(null, {
       statusCode: 404,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'content-type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "content-type": "application/json",
       },
-      body: JSON.stringify({errorMessage: errorMessage}),
+      body: JSON.stringify({ errorMessage: errorMessage }),
     });
   });
 
-  test('Throws Error', async () => {
-    const errorMessage = 'generic error message';
+  test("Throws Error", async () => {
+    const errorMessage = "generic error message";
     mockGetCategoryListResponse.mockImplementation(() => {
       throw new Error(errorMessage);
     });
@@ -68,17 +69,17 @@ describe('List Categorys', () => {
     expect(callback).toHaveBeenCalledWith(null, {
       statusCode: 500,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'content-type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "content-type": "application/json",
       },
-      body: JSON.stringify({errorMessage: errorMessage}),
+      body: JSON.stringify({ errorMessage: errorMessage }),
     });
   });
 });
 
 const toEvent = (id: Uuid): any => ({
   pathParameters: {
-    hackathonId: id,
+    id: id,
   },
 });
--- hms-api/__tests__/handler/participant/list.test.ts
+++ hms-api/__tests__/handler/participant/list.test.ts
@@ -1,32 +1,39 @@
 import {
   makeParticipant,
   ParticipantData,
-} from '../../repository/domain/participant-maker';
-import {list} from '../../../src/handler/participant/list';
-import {randomUser} from '../../repository/domain/user-maker';
-import Uuid, {uuid} from '../../../src/util/Uuid';
-import NotFoundError from '../../../src/error/NotFoundError';
-import ParticipantListResponse from '../../../src/rest/ParticipantListResponse';
-import ReferenceNotFoundError from '../../../src/error/ReferenceNotFoundError';
-import * as participantService from '../../../src/service/participant-service';
+} from "../../repository/domain/participant-maker";
+import { list } from "../../../src/handler/participant/list";
+import { randomUser } from "../../repository/domain/user-maker";
+import Uuid, { uuid } from "../../../src/util/Uuid";
+import NotFoundError from "../../../src/error/NotFoundError";
+import ParticipantListResponse from "../../../src/rest/ParticipantListResponse";
+import ReferenceNotFoundError from "../../../src/error/ReferenceNotFoundError";
+import * as participantService from "../../../src/service/participant-service";
 
 const mockGetParticipantListResponse = jest.fn();
-jest.spyOn(participantService, 'getParticipantListResponse')
-    .mockImplementation(mockGetParticipantListResponse);
+jest
+  .spyOn(participantService, "getParticipantListResponse")
+  .mockImplementation(mockGetParticipantListResponse);
 
-describe('List Participants', () => {
-  test('Happy Path', async () => {
+describe("List Participants", () => {
+  test("Happy Path", async () => {
     const hackathonId = uuid();
     const user1 = randomUser();
-    const participant1 = makeParticipant({userId: user1.id} as ParticipantData);
+    const participant1 = makeParticipant({
+      userId: user1.id,
+    } as ParticipantData);
     const user2 = randomUser();
-    const participant2 = makeParticipant({userId: user2.id} as ParticipantData);
+    const participant2 = makeParticipant({
+      userId: user2.id,
+    } as ParticipantData);
     const user3 = randomUser();
-    const participant3 = makeParticipant({userId: user3.id} as ParticipantData);
+    const participant3 = makeParticipant({
+      userId: user3.id,
+    } as ParticipantData);
     const expected = ParticipantListResponse.from(
-        [participant1, participant2, participant3],
-        [user1, user2, user3],
-        hackathonId,
+      [participant1, participant2, participant3],
+      [user1, user2, user3],
+      hackathonId
     );
 
     mockGetParticipantListResponse.mockResolvedValue(expected);
@@ -38,16 +45,16 @@ describe('List Participants', () => {
     expect(callback).toHaveBeenCalledWith(null, {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'content-type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "content-type": "application/json",
       },
       body: JSON.stringify(expected),
     });
   });
 
-  test('Throws NotFoundError', async () => {
-    const errorMessage = 'reference error message';
+  test("Throws NotFoundError", async () => {
+    const errorMessage = "reference error message";
     mockGetParticipantListResponse.mockImplementation(() => {
       throw new NotFoundError(errorMessage);
     });
@@ -57,16 +64,16 @@ describe('List Participants', () => {
     expect(callback).toHaveBeenCalledWith(null, {
       statusCode: 404,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'content-type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "content-type": "application/json",
       },
-      body: JSON.stringify({errorMessage: errorMessage}),
+      body: JSON.stringify({ errorMessage: errorMessage }),
     });
   });
 
-  test('Throws ReferenceNotFoundError', async () => {
-    const errorMessage = 'reference error message';
+  test("Throws ReferenceNotFoundError", async () => {
+    const errorMessage = "reference error message";
     mockGetParticipantListResponse.mockImplementation(() => {
       throw new ReferenceNotFoundError(errorMessage);
     });
@@ -76,16 +83,16 @@ describe('List Participants', () => {
     expect(callback).toHaveBeenCalledWith(null, {
       statusCode: 400,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'content-type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "content-type": "application/json",
       },
-      body: JSON.stringify({errorMessage: errorMessage}),
+      body: JSON.stringify({ errorMessage: errorMessage }),
     });
   });
 
-  test('Throws Error', async () => {
-    const errorMessage = 'generic error message';
+  test("Throws Error", async () => {
+    const errorMessage = "generic error message";
     mockGetParticipantListResponse.mockImplementation(() => {
       throw new Error(errorMessage);
     });
@@ -95,18 +102,17 @@ describe('List Participants', () => {
     expect(callback).toHaveBeenCalledWith(null, {
       statusCode: 500,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'content-type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "content-type": "application/json",
       },
-      body: JSON.stringify({errorMessage: errorMessage}),
+      body: JSON.stringify({ errorMessage: errorMessage }),
     });
   });
 });
 
 const toEvent = (id: Uuid): any => ({
   pathParameters: {
-    hackathonId: id,
+    id: id,
   },
 });
-
