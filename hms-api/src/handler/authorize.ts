e56f9baceabccb0bcdeb54f9aa489b50d9ef4eb0
verzano <mitchell.v.berg@gmail.com>
2022-07-27 11:33:38 +0200

    just updating some files to be in line with the rest of the backend

--- hms-api/src/handler/authorize.ts
+++ hms-api/src/handler/authorize.ts
@@ -1,5 +1,6 @@
-import { alwaysPass } from "./allPassAuthorizer";
-import { authorizeWithActiveDirectory } from "./activeDirectoryAuthorizer";
+/* eslint-disable require-jsdoc */
+import {alwaysPass} from './all-pass-authorizer';
+import {authorizeWithActiveDirectory} from './active-directory-authorizer';
 
 const AUTHORIZER_TYPE = process.env.AUTHORIZER_TYPE || 'ALWAYS_PASS';
 
