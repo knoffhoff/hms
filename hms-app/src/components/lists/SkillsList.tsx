eaafa9908c2c01c95d4ac5d0c2de8c48885052b4
Jakob Panten <jakob@panten-sh.de>
2022-08-25 11:55:25 +0200

    fix merge conflicts

--- hms-app/src/components/lists/SkillsList.tsx
+++ hms-app/src/components/lists/SkillsList.tsx
@@ -1,82 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import { useMsal } from '@azure/msal-react';
+import React, { useEffect, useState } from 'react'
+import { useMsal } from '@azure/msal-react'
 
-import { styles } from '../../common/styles';
-import { SkillPreview } from '../../common/types';
-import { getListOfSkills } from '../../actions/SkillActions';
-import { Accordion, Button, Card, Group } from '@mantine/core';
-import SkillDetails from '../card-details/SkillDetails';
-import SkillForm from '../input-forms/SkillForm';
-
+import { styles } from '../../common/styles'
+import { SkillPreview } from '../../common/types'
+import { getListOfSkills } from '../../actions/SkillActions'
+import { Accordion, Button, Card, Group } from '@mantine/core'
+import SkillDetails from '../card-details/SkillDetails'
+import SkillForm from '../input-forms/SkillForm'
 
 const SkillsList = (): React.ReactElement => {
-    const { instance } = useMsal();
-    const { classes } = styles();
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [skillsList, setSkillsList] = useState([] as SkillPreview[]);
+  const { instance } = useMsal()
+  const { classes } = styles()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [skillsList, setSkillsList] = useState([] as SkillPreview[])
 
-    const loadSkills = () => {
-        setIsLoading(true);
-        getListOfSkills(instance)
-            .then((data) => {
-                let skills = [] as SkillPreview[];
-                if (data && data.skills) {
-                    skills = data.skills;
-                }
-                setSkillsList(skills);
-                setIsLoading(false);
-            })
-            .finally(() => setIsLoading(false));
-    };
+  const loadSkills = () => {
+    setIsLoading(true)
+    getListOfSkills(instance)
+      .then((data) => {
+        let skills = [] as SkillPreview[]
+        if (data && data.skills) {
+          skills = data.skills
+        }
+        setSkillsList(skills)
+        setIsLoading(false)
+      })
+      .finally(() => setIsLoading(false))
+  }
 
-    useEffect(() => {
-        loadSkills();
-    }, []);
+  useEffect(() => {
+    loadSkills()
+  }, [])
 
-    const refreshList = (): void => {
-        setIsLoading(true);
-        loadSkills();
-    };
+  const refreshList = (): void => {
+    setIsLoading(true)
+    loadSkills()
+  }
 
-    const allSkills = skillsList?.map((skill, index) => (
-        <Accordion.Item
-            key={index}
-            label={
-                <div>
-                    {index + 1}. {skill.name}
-                </div>
-            }
-        >
-            <SkillDetails skillId={skill.id.toString()} />
-        </Accordion.Item>
-    ));
+  const allSkills = skillsList?.map((skill, index) => (
+    <Accordion.Item key={index} value={skill.id}>
+      <Accordion.Control>
+        <div>
+          {index + 1}. {skill.name}
+        </div>
+      </Accordion.Control>
+      <Accordion.Panel>
+        <SkillDetails skillId={skill.id.toString()} />
+      </Accordion.Panel>
+    </Accordion.Item>
+  ))
 
-    return (
-        <>
-            {!isLoading && <Card withBorder className={classes.card}>
-                <Card.Section className={classes.borderSection}>
-                    <Group position='left' mt='xl'>
-                        {!isLoading && <Button onClick={refreshList}>Refresh list</Button>}
-                        {isLoading && <div>Loading...</div>}
-                    </Group>
-                </Card.Section>
-                <Card.Section>
-                    <Accordion iconPosition='right'>
-                        <Accordion.Item
-                            className={classes.borderAccordion}
-                            label={'Add Skill'}
-                        >
-                            <SkillForm
-                                context={'new'}
-                                skillId={''}
-                            />
-                        </Accordion.Item>
-                        {allSkills}
-                    </Accordion>
-                </Card.Section>
-            </Card>}{' '}
-        </>
-    );
+  return (
+    <>
+      {!isLoading && (
+        <Card withBorder className={classes.card}>
+          <Card.Section className={classes.borderSection}>
+            <Group position='left' mt='xl'>
+              {!isLoading && (
+                <Button onClick={refreshList}>Refresh list</Button>
+              )}
+              {isLoading && <div>Loading...</div>}
+            </Group>
+          </Card.Section>
+          <Card.Section>
+            <Accordion chevronPosition={'right'}>
+              <Accordion.Item
+                className={classes.borderAccordion}
+                value={'ad-skills'}
+              >
+                <Accordion.Control>Add Skill</Accordion.Control>
+                <Accordion.Panel>
+                  <SkillForm context={'new'} skillId={''} />
+                </Accordion.Panel>
+              </Accordion.Item>
+              {allSkills}
+            </Accordion>
+          </Card.Section>
+        </Card>
+      )}{' '}
+    </>
+  )
 }
 
-export default SkillsList;
+export default SkillsList
