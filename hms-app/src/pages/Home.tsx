12f9f36768a7cf3054d6dd96b14f24175b77e90d
Marcel Kühnau <59581396+LetsPoke@users.noreply.github.com>
2022-10-26 16:49:48 +0200

    Remove unnecessary hackathon state code (#808)

--- hms-app/src/pages/Home.tsx
+++ hms-app/src/pages/Home.tsx
@@ -1,107 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import {
-  Stepper,
-  Title,
-  Text,
-  Container,
-  SimpleGrid,
-  Card,
-  Center,
-  useMantineColorScheme,
-} from '@mantine/core'
-import { styles } from '../common/styles'
+import React from 'react'
+import { Title, Text, Container, SimpleGrid, Card } from '@mantine/core'
 import { useAppSelector } from '../hooks'
 import { qAndAList } from '../common/HomeQandAContent'
 import HeroHeader from '../components/HeroHeader'
 
 function Home() {
-  const theme = useMantineColorScheme()
-  const { classes } = styles()
-  const [active, setActive] = useState(0)
-  const [timeUntilNextHackathon, setTimeUntilNextHackathon] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  })
-  const [registration, setRegistration] = useState({
-    openDate: new Date(),
-    closeDate: new Date(),
-  })
-
   const nextHackathon = useAppSelector(
     (state) => state.hackathons.nextHackathon
-  )
-
-  useEffect(() => {
-    setTimeUntilNextHackathon(getTimeDifferenceToNow(nextHackathon.startDate))
-    const openDate = new Date(nextHackathon.startDate)
-    openDate.setDate(openDate.getDate() - 10)
-    setRegistration({
-      openDate: openDate,
-      closeDate: new Date(nextHackathon.startDate),
-    })
-  }, [nextHackathon])
-
-  const getTimeDifferenceToNow = (dateString: string) => {
-    const date = new Date(dateString)
-    const now = new Date()
-    const difference = date.getTime() - now.getTime()
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24))
-    const hours = Math.floor(
-      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    )
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000)
-    return { days, hours, minutes, seconds }
-  }
-
-  const today = new Date()
-  today.setHours(0, 0, 0, 0)
-
-  function setActiveTimeline() {
-    // ToDo: access Hackathon status and set active state
-    if (registration.openDate < today && registration.closeDate > today) {
-      setActive(1)
-    } else if (
-      registration.closeDate < today &&
-      new Date(nextHackathon.startDate) > today
-    ) {
-      setActive(2)
-    } else if (
-      new Date(nextHackathon.startDate) < today &&
-      new Date(nextHackathon.endDate) > today
-    ) {
-      setActive(3)
-    } else {
-      setActive(0)
-    }
-  }
-
-  useEffect(() => {
-    setActiveTimeline()
-  }, [nextHackathon])
-
-  const timelineStepper = (
-    <Stepper iconSize={35} active={active} breakpoint='sm' py={50}>
-      <Stepper.Step
-        className={classes.stepperStep}
-        label={'Registration and Idea submission open '}
-      />
-      <Stepper.Step
-        className={classes.stepperStep}
-        label={'Registration and Idea submission closed! '}
-      />
-      <Stepper.Step
-        className={classes.stepperStep}
-        label={'Hackathon Started'}
-      />
-      <Stepper.Step className={classes.stepperStep} label={'Voting Open'} />
-      <Stepper.Step
-        className={classes.stepperStep}
-        label={'Voting and Hackathon Ended'}
-      />
-    </Stepper>
   )
 
   return (
