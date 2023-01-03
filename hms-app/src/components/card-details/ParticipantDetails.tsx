0552d434a1f673fa5a5e36b521c287a344027b63
MarcelK <59581396+LetsPoke@users.noreply.github.com>
2023-01-03 09:44:36 +0100

    add email to partcipant info

--- hms-app/src/components/card-details/ParticipantDetails.tsx
+++ hms-app/src/components/card-details/ParticipantDetails.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
-import { UserPreview } from '../../common/types'
+import React, { useEffect, useState } from 'react'
+import { User, UserPreview } from '../../common/types'
 import { Button, Card, Group, Modal, Text } from '@mantine/core'
 import { deleteParticipant } from '../../actions/ParticipantActions'
 import { styles } from '../../common/styles'
 import { useMsal } from '@azure/msal-react'
 import { DELETE_BUTTON_COLOR } from '../../common/colors'
+import { getUserDetails } from '../../actions/UserActions'
 
 type IProps = {
   participantId: string
@@ -16,12 +17,25 @@ export default function ParticipantDetails(props: IProps) {
   const { classes } = styles()
   const { participantId, user } = props
   const [opened, setOpened] = useState(false)
+  const [userDetails, setUserDetails] = useState({} as User)
+
+  const loadUserDetails = () => {
+    if (user.id !== '') {
+      getUserDetails(instance, user.id).then((data) => {
+        setUserDetails(data)
+      })
+    }
+  }
 
   const deleteSelectedParticipant = () => {
     deleteParticipant(instance, participantId).then(() => {
       setOpened(false)
     })
   }
+
+  useEffect(() => {
+    loadUserDetails()
+  }, [participantId])
 
   return (
     <>
@@ -36,7 +50,7 @@ export default function ParticipantDetails(props: IProps) {
 
         <Card.Section className={classes.borderSection}>
           <Text className={classes.label}>E-mail:</Text>
-          <Text className={classes.text}>email needs to be added</Text>
+          <Text className={classes.text}>{userDetails.emailAddress}</Text>
         </Card.Section>
 
         <Card.Section className={classes.borderSection}>
