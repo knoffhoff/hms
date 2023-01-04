079c70ad6dca07bad19d44dfe9da1104c653d3b6
Verzano <mitchell.v.berg@gmail.com>
2023-01-04 13:38:27 +0100

    Making github test for me cause it's faster

--- hms-api/__tests__/service/idea-service.test.ts
+++ hms-api/__tests__/service/idea-service.test.ts
@@ -533,7 +533,7 @@ describe('Get Idea Response', () => {
     );
     mockResolvedValues(idea, ownerUser, participantUsers, participants, voterUsers, voters, hackathon, skills, category);
 
-    // expect(await getIdeaResponse(idea.id)).toStrictEqual(expected);
+    await getIdeaResponse(idea.id);
     expect(mockGetIdea).toHaveBeenCalledWith(idea.id);
     expect(mockGetUser).toHaveBeenCalledWith(idea.ownerId);
     expect(mockGetParticipants).toHaveBeenCalledWith(idea.participantIds);
