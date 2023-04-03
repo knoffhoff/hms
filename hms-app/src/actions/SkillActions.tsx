b542d19c93dbdd2bdcb67d9e766b66cb299c6edb
MarcelK <59581396+LetsPoke@users.noreply.github.com>
2023-04-03 14:48:33 +0200

    fixed indent and semicolons

--- hms-app/src/actions/SkillActions.tsx
+++ hms-app/src/actions/SkillActions.tsx
@@ -5,79 +5,77 @@ import { buildFetchOptions } from '../common/actionOptions'
 const coreUrl = process.env.REACT_APP_CORE_URL
 
 export const addSkill = async (
-    instance: IPublicClientApplication,
-    props: {
-        name: string,
-        description: string,
-    }
+  instance: IPublicClientApplication,
+  props: {
+    name: string
+    description: string
+  }
 ) => {
-    const idToken = await getIdToken(instance);
-    const options = buildFetchOptions('POST', idToken, props);
+  const idToken = await getIdToken(instance)
+  const options = buildFetchOptions('POST', idToken, props)
 
-    return fetch(`${coreUrl}/skill`, options)
-        .then((response) => {
-            return response.json()
-        })
-        .catch((err) => console.error(err));
+  return fetch(`${coreUrl}/skill`, options)
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => console.error(err))
 }
 
-export const getListOfSkills = async (
-    instance: IPublicClientApplication
-) => {
-    const idToken = await getIdToken(instance);
-    const options = buildFetchOptions('GET', idToken);
-    return fetch(`${coreUrl}/skills`, options)
-        .then((data) => {
-            return data.json()
-        })
-        .catch((err) => console.log(err));
+export const getListOfSkills = async (instance: IPublicClientApplication) => {
+  const idToken = await getIdToken(instance)
+  const options = buildFetchOptions('GET', idToken)
+  return fetch(`${coreUrl}/skills`, options)
+    .then((data) => {
+      return data.json()
+    })
+    .catch((err) => console.log(err))
 }
 
 export const getSkillDetails = async (
-    instance: IPublicClientApplication,
-    skillId: string
+  instance: IPublicClientApplication,
+  skillId: string
 ) => {
-    const idToken = await getIdToken(instance);
-    const options = buildFetchOptions('GET', idToken);
+  const idToken = await getIdToken(instance)
+  const options = buildFetchOptions('GET', idToken)
 
-    return fetch(`${coreUrl}/skill/${skillId}`, options)
-        .then((data) => {
-            return data.json()
-        })
-        .catch((err) => console.log(err));
+  return fetch(`${coreUrl}/skill/${skillId}`, options)
+    .then((data) => {
+      return data.json()
+    })
+    .catch((err) => console.log(err))
 }
 
 export const editSkill = async (
-    instance: IPublicClientApplication,
-    props: {
-        id: string,
-        name: string,
-        description: string,
-    }
+  instance: IPublicClientApplication,
+  props: {
+    id: string
+    name: string
+    description: string
+  }
 ) => {
-    const idToken = await getIdToken(instance);
-    const options = buildFetchOptions('PUT', idToken, {
-        name: props.name,
-        description: props.description
-    });
+  const idToken = await getIdToken(instance)
+  const options = buildFetchOptions('PUT', idToken, {
+    name: props.name,
+    description: props.description,
+  })
 
-    return fetch(`${coreUrl}/skill/${props.id}`, options)
-        .then((response) => {
-            return response.json()
-        })
-        .catch((err) => console.log(err));
+  return fetch(`${coreUrl}/skill/${props.id}`, options)
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => console.log(err))
 }
 
 export const removeSkill = async (
-    instance: IPublicClientApplication,
-    skillId: string
+  instance: IPublicClientApplication,
+  skillId: string
 ) => {
-    const idToken = await getIdToken(instance);
-    const options = buildFetchOptions('DELETE', idToken);
+  const idToken = await getIdToken(instance)
+  const options = buildFetchOptions('DELETE', idToken)
 
-    return fetch(`${coreUrl}/skill/${skillId}`, options)
-        .then((response) => {
-            return response.json()
-        })
-        .catch((err) => console.log(err));
-};
+  return fetch(`${coreUrl}/skill/${skillId}`, options)
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => console.log(err))
+}
--- hms-app/src/components/card-details/SkillDetails.tsx
+++ hms-app/src/components/card-details/SkillDetails.tsx
@@ -1,147 +1,143 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Card, Group, Modal, Text } from '@mantine/core';
-import { useMsal } from '@azure/msal-react';
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Group, Modal, Text } from '@mantine/core'
+import { useMsal } from '@azure/msal-react'
 
-import { getSkillDetails, removeSkill } from '../../actions/SkillActions';
-import { DELETE_BUTTON_COLOR, JOIN_BUTTON_COLOR } from '../../common/colors';
-import { styles } from '../../common/styles';
-import { Skill } from '../../common/types';
-import SkillForm from '../input-forms/SkillForm';
-
-
+import { getSkillDetails, removeSkill } from '../../actions/SkillActions'
+import { DELETE_BUTTON_COLOR, JOIN_BUTTON_COLOR } from '../../common/colors'
+import { styles } from '../../common/styles'
+import { Skill } from '../../common/types'
+import SkillForm from '../input-forms/SkillForm'
 
 type IProps = {
-    skillId: string
+  skillId: string
 }
 
 const SkillDetails = (props: IProps): React.ReactElement => {
-    const { instance } = useMsal();
-    const { classes } = styles();
-    const { skillId } = props;
-    const [deleteModalOpened, setDeleteModalOpened] = useState(false);
-    const [editModalOpened, setEditModalOpened] = useState(false);
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [skillData, setSkillData] = useState({
-        id: '',
-        name: '',
-        description: '',
-    } as Skill);
+  const { instance } = useMsal()
+  const { classes } = styles()
+  const { skillId } = props
+  const [deleteModalOpened, setDeleteModalOpened] = useState(false)
+  const [editModalOpened, setEditModalOpened] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [skillData, setSkillData] = useState({
+    id: '',
+    name: '',
+    description: '',
+  } as Skill)
 
-    const loadSelectedSkill = (): void => {
-        getSkillDetails(instance, skillId).then(
-            (data) => {
-                setIsError(false);
-                setIsLoading(false);
-                setSkillData(data);
-            },
-            () => {
-                setIsError(true);
-                setIsLoading(false);
-            }
-        );
-    };
+  const loadSelectedSkill = (): void => {
+    getSkillDetails(instance, skillId).then(
+      (data) => {
+        setIsError(false)
+        setIsLoading(false)
+        setSkillData(data)
+      },
+      () => {
+        setIsError(true)
+        setIsLoading(false)
+      }
+    )
+  }
 
-    const deleteSelectedSkill = () => {
-        removeSkill(instance, skillId).then(() => {
-            setDeleteModalOpened(false)
-        });
-    };
+  const deleteSelectedSkill = () => {
+    removeSkill(instance, skillId).then(() => {
+      setDeleteModalOpened(false)
+    })
+  }
 
-    useEffect(() => {
-        loadSelectedSkill();
-        setIsLoading(true);
-    }, []);
+  useEffect(() => {
+    loadSelectedSkill()
+    setIsLoading(true)
+  }, [])
 
-    const deleteModal = (
-        <Modal
-            centered
-            opened={deleteModalOpened}
-            onClose={() => setDeleteModalOpened(false)}
-            withCloseButton={false}
+  const deleteModal = (
+    <Modal
+      centered
+      opened={deleteModalOpened}
+      onClose={() => setDeleteModalOpened(false)}
+      withCloseButton={false}
+    >
+      <Text className={classes.text}>
+        Are you sure you want to delete this skill?
+      </Text>
+      <Text className={classes.title}>Name: {skillData.name}</Text>
+      {!isLoading && (
+        <Button
+          style={{ backgroundColor: DELETE_BUTTON_COLOR }}
+          onClick={() => deleteSelectedSkill()}
         >
-            <Text className={classes.text}>
-                Are you sure you want to delete this skill?
-            </Text>
-            <Text className={classes.title}>Name: {skillData.name}</Text>
-            {!isLoading && (
-                <Button
-                    style={{ backgroundColor: DELETE_BUTTON_COLOR }}
-                    onClick={() => deleteSelectedSkill()}
-                >
-                    Yes delete this skill
-                </Button>
-            )}
-            <Text className={classes.text}>
-                (This window will automatically close as soon as the skill is
-                deleted)
-            </Text>
-        </Modal>
-    );
+          Yes delete this skill
+        </Button>
+      )}
+      <Text className={classes.text}>
+        (This window will automatically close as soon as the skill is deleted)
+      </Text>
+    </Modal>
+  )
 
-    const editModal = (
-        <Modal
-            centered
-            opened={editModalOpened}
-            onClose={() => setEditModalOpened(false)}
-            withCloseButton={false}
-        >
-            <Text className={classes.title}>Edit Skill</Text>
-            <SkillForm skillId={skillId} context={'edit'} />
-            {isLoading && <div>Loading...</div>}
-            <Text className={classes.text}>
-                (This window will automatically close as soon as the skill is
-                changed)
-            </Text>
-        </Modal>
-    );
+  const editModal = (
+    <Modal
+      centered
+      opened={editModalOpened}
+      onClose={() => setEditModalOpened(false)}
+      withCloseButton={false}
+    >
+      <Text className={classes.title}>Edit Skill</Text>
+      <SkillForm skillId={skillId} context={'edit'} />
+      {isLoading && <div>Loading...</div>}
+      <Text className={classes.text}>
+        (This window will automatically close as soon as the skill is changed)
+      </Text>
+    </Modal>
+  )
 
-    return (
-        <>
-            {isError && !isLoading && (
-                <div>
-                    <Text className={classes.title}>Error loading Skill</Text>
-                    <Text className={classes.text}>something went wrong.</Text>
-                </div>
-            )}
-            {isLoading && !isError && (
-                <div>
-                    <Text className={classes.text}>Skill details are loading...</Text>
-                </div>
-            )}
+  return (
+    <>
+      {isError && !isLoading && (
+        <div>
+          <Text className={classes.title}>Error loading Skill</Text>
+          <Text className={classes.text}>something went wrong.</Text>
+        </div>
+      )}
+      {isLoading && !isError && (
+        <div>
+          <Text className={classes.text}>Skill details are loading...</Text>
+        </div>
+      )}
 
-            {!isLoading && !isError && (
-                <Card withBorder className={classes.card}>
-                    <Card.Section className={classes.borderSection}>
-                        <Text className={classes.title}>{skillData.name}</Text>
-                        <Text className={classes.text}>ID: {skillData.id}</Text>
-                    </Card.Section>
-                    <Card.Section className={classes.borderSection}>
-                        <Text className={classes.label}>Description:</Text>
-                        <Text className={classes.text}>{skillData.description}</Text>
-                    </Card.Section>
-                    <Card.Section className={classes.borderSection}>
-                        <Group position='left' mt='xl'>
-                            {deleteModal}
-                            <Button
-                                style={{ backgroundColor: DELETE_BUTTON_COLOR }}
-                                onClick={() => setDeleteModalOpened(true)}
-                            >
-                                Delete
-                            </Button>
-                            {editModal}
-                            <Button
-                                style={{ backgroundColor: JOIN_BUTTON_COLOR }}
-                                onClick={() => setEditModalOpened(true)}
-                            >
-                                Edit
-                            </Button>
-                        </Group>
-                    </Card.Section>
-                </Card>
-            )}
-        </>
-    );
-};
+      {!isLoading && !isError && (
+        <Card withBorder className={classes.card}>
+          <Card.Section className={classes.borderSection}>
+            <Text className={classes.title}>{skillData.name}</Text>
+            <Text className={classes.text}>ID: {skillData.id}</Text>
+          </Card.Section>
+          <Card.Section className={classes.borderSection}>
+            <Text className={classes.label}>Description:</Text>
+            <Text className={classes.text}>{skillData.description}</Text>
+          </Card.Section>
+          <Card.Section className={classes.borderSection}>
+            <Group position='left' mt='xl'>
+              {deleteModal}
+              <Button
+                style={{ backgroundColor: DELETE_BUTTON_COLOR }}
+                onClick={() => setDeleteModalOpened(true)}
+              >
+                Delete
+              </Button>
+              {editModal}
+              <Button
+                style={{ backgroundColor: JOIN_BUTTON_COLOR }}
+                onClick={() => setEditModalOpened(true)}
+              >
+                Edit
+              </Button>
+            </Group>
+          </Card.Section>
+        </Card>
+      )}
+    </>
+  )
+}
 
-export default SkillDetails;
+export default SkillDetails
