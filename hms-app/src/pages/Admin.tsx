d6b0fc4c08d05c910ad5d31ffa2bc5e754aa5aa3
Reyhan Arkin Yosua Rumengan <reyhan.rumengan@idealo.de>
2023-05-02 14:40:07 +0200

    auto close accordion

--- hms-app/src/pages/Admin.tsx
+++ hms-app/src/pages/Admin.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Accordion, Title } from '@mantine/core'
 import HackathonForm from '../components/input-forms/HackathonForm'
 import AllHackathonList from '../components/lists/AllHackathonList'
@@ -8,10 +8,15 @@ import SkillsList from '../components/lists/SkillsList'
 
 function Admin() {
   const { classes } = styles()
+  const [openedAccordion, setOpenedAccordion] = useState<string | null>(null)
 
   const accordion = (value: string, title: string, element: JSX.Element) => {
     return (
-      <Accordion className={classes.accordionList}>
+      <Accordion
+        className={classes.accordionList}
+        value={openedAccordion}
+        onChange={setOpenedAccordion}
+      >
         <Accordion.Item className={classes.borderAccordion} value={value}>
           <Accordion.Control>{title}</Accordion.Control>
           <Accordion.Panel>{element}</Accordion.Panel>
