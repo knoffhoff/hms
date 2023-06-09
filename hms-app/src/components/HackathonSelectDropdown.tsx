d0f93df3af4b210454adaea0518e5e099e8920d8
katty-bird <ekaterina.kuritsyna@idealo.de>
2023-06-09 09:14:12 +0200

    revert changes

--- hms-app/src/components/HackathonSelectDropdown.tsx
+++ hms-app/src/components/HackathonSelectDropdown.tsx
@@ -72,19 +72,12 @@ export default function HackathonSelectDropdown({
           .map((hackathon) => mapHackathonToSelectItem(hackathon))
       case HackathonDropdownMode.MoveModal:
         return hackathonList.map((hackathon) =>
-          mapHackathonToSelectItemWithoutDate(hackathon)
+          mapHackathonToSelectItem(hackathon)
         )
     }
     return hackathonList
       .filter((hackathon) => hackathon.endDate > MIN_DATE)
       .map((hackathon) => mapHackathonToSelectItem(hackathon))
-  }
-
-  function mapHackathonToSelectItemWithoutDate(hackathon: HackathonPreview): SelectItem {
-    return {
-      value: hackathon.id,
-      label: hackathon.title,
-    }
   }
 
   function mapHackathonToSelectItem(hackathon: HackathonPreview): SelectItem {
