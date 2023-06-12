f273fe8dac040ae48c16a7b8b3860515841aef2b
Reyhan Arkin Yosua Rumengan <reyhan.rumengan@idealo.de>
2023-06-12 11:52:25 +0200

    remove date badge

--- hms-app/src/components/HackathonHeader.tsx
+++ hms-app/src/components/HackathonHeader.tsx
@@ -35,26 +35,6 @@ export default function HackathonHeader(props: IProps) {
             />
           </Center>
         )}
-        <Center mt={10}>
-          <Badge
-            size={'lg'}
-            variant={'gradient'}
-            gradient={
-              theme.colorScheme === 'dark'
-                ? { from: 'teal', to: 'blue', deg: 60 }
-                : { from: '#ed6ea0', to: '#ec8c69', deg: 35 }
-            }
-            radius={'xs'}
-          >
-            {isHackathonWithoutDate()
-              ? 'To be announced'
-              : `${new Date(
-                  hackathonData.startDate
-                ).toLocaleDateString()} - ${new Date(
-                  hackathonData.endDate
-                ).toLocaleDateString()}`}
-          </Badge>
-        </Center>
       </div>
     </Container>
   )
