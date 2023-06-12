b7af6bdc3eb3e4148f7276bb5236834e30275d89
MarcelK <59581396+LetsPoke@users.noreply.github.com>
2023-06-12 17:07:18 +0200

    Updated Landing Page V1

--- hms-app/src/components/HeroHeader.tsx
+++ hms-app/src/components/HeroHeader.tsx
@@ -1,30 +1,29 @@
 import {
-  Badge,
   Button,
-  Center,
+  Card,
   Container,
   createStyles,
+  SimpleGrid,
   Text,
   Title,
   useMantineColorScheme,
 } from '@mantine/core'
-import { Dots } from './Dots'
 import Countdown from 'react-countdown'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { HackathonSerializable } from '../common/types'
 import { blue3, orange3 } from '../common/colors'
 import { Link } from 'react-router-dom'
-import { MAX_DATE } from '../common/constants'
+import { MIN_DATE } from '../common/constants'
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
     position: 'relative',
-    paddingTop: 120,
-    paddingBottom: 80,
+    paddingTop: 60, // from 120
+    paddingBottom: 40, // from 80
 
     '@media (max-width: 755px)': {
-      paddingTop: 80,
-      paddingBottom: 60,
+      paddingTop: 40, // from 80
+      paddingBottom: 30, // from 60
     },
   },
 
@@ -61,6 +60,17 @@ const useStyles = createStyles((theme) => ({
 
     '@media (max-width: 520px)': {
       fontSize: 28,
+      textAlign: 'left',
+    },
+  },
+
+  smallerText: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+    '@media (max-width: 520px)': {
+      fontSize: 18,
       textAlign: 'left',
     },
   },
@@ -103,115 +113,134 @@ const useStyles = createStyles((theme) => ({
       },
     },
   },
+
+  centeredText: {
+    textAlign: 'center',
+    marginTop: '20px',
+  },
 }))
 
-const HeroHeader = (props: { nextHackathon: HackathonSerializable }) => {
+const InfoCard = (props: {
+  title: string
+  description: string
+  link: string
+  buttonText: string
+}) => {
   const theme = useMantineColorScheme()
   const { classes } = useStyles()
-  const today = new Date()
-  today.setHours(0, 0, 0, 0)
-  const isHackathonWithoutDate = () =>
-    new Date(props.nextHackathon.startDate) > MAX_DATE
+  return (
+    <Card withBorder shadow='sm' p='lg'>
+      <Title align={'center'} order={3}>
+        {props.title}
+      </Title>
+      <Text align={'center'}>{props.description}</Text>
+      <div className={classes.centeredText}>
+        <Button
+          size='xs'
+          component={Link}
+          to={props.link}
+          color={theme.colorScheme === 'dark' ? 'orange' : 'blue'}
+        >
+          {props.buttonText}
+        </Button>
+      </div>
+    </Card>
+  )
+}
+
+const HeroHeader = (props: {
+  nextHackathon: HackathonSerializable
+  lastHackathon: HackathonSerializable
+}) => {
+  const { classes } = useStyles()
+
+  // Using useMemo for performance
+  const today = useMemo(() => {
+    const date = new Date()
+    date.setHours(0, 0, 0, 0)
+    return date
+  }, [])
 
   return (
     <Container className={classes.wrapper} size={1400}>
-      <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
-      <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
-      <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
-      <Dots className={classes.dots} style={{ right: 0, top: 60 }} />
-
       <div className={classes.inner}>
-        {!isHackathonWithoutDate() ? (
-          <Title className={classes.title}>
-            Upcoming{' '}
-            <Text component='span' className={classes.highlight} inherit>
-              {props.nextHackathon.title}
-            </Text>
-            {new Date(props.nextHackathon.startDate) > today ? (
-              <>
-                {' '}
-                starts in
-                <Center styles={{ color: 'red' }}>
-                  {today < new Date(props.nextHackathon.startDate) && (
-                    <Countdown date={props.nextHackathon.startDate} />
-                  )}
-                </Center>
-              </>
-            ) : (
-              <>
-                {' '}
-                ends in
-                <Center styles={{ color: 'red' }}>
-                  {today < new Date(props.nextHackathon.endDate) && (
-                    <Countdown date={props.nextHackathon.endDate} />
-                  )}
-                </Center>
-              </>
-            )}
-          </Title>
-        ) : (
-          <Title className={classes.title}>
-            Upcoming{' '}
-            <Text component='span' className={classes.highlight} inherit>
-              {props.nextHackathon.title}
-            </Text>
-          </Title>
-        )}
-
-        <Center my={25}>
-          <Badge
-            size={'xl'}
-            variant='gradient'
-            gradient={
-              theme.colorScheme === 'dark'
-                ? { from: 'teal', to: 'blue', deg: 60 }
-                : { from: '#ed6ea0', to: '#ec8c69', deg: 35 }
-            }
-            radius={'xs'}
-          >
-            {isHackathonWithoutDate()
-              ? 'To be announced'
-              : `${new Date(
-                  props.nextHackathon.startDate
-                ).toLocaleDateString()} - ${new Date(
-                  props.nextHackathon.endDate
-                ).toLocaleDateString()}`}
-          </Badge>
-        </Center>
-
-        <Container p={0} size={600}>
-          <Text
-            size='lg'
-            color='dimmed'
-            className={classes.description}
-            dangerouslySetInnerHTML={{
-              __html: props.nextHackathon.description || '',
-            }}
-          />
-        </Container>
-
-        <div className={classes.controls}>
-          <Button
-            component={Link}
-            to='/hackathons'
-            className={classes.control}
-            size='lg'
-            variant='default'
-            color='gray'
-          >
-            Explore ideas
-          </Button>
-          <Button
-            className={classes.control}
-            size='lg'
-            component={Link}
-            to='/my-ideas'
-            color={theme.colorScheme === 'dark' ? 'orange' : 'blue'}
-          >
-            Submit project idea
-          </Button>
-        </div>
+        <Title className={classes.title} order={2} align={'center'}>
+          Welcome to the Ideation Portal
+        </Title>
+        <Text align={'center'} mb={20}>
+          Your digital space for collecting, sharing, and exploring ideas
+        </Text>
       </div>
+
+      {!props.nextHackathon || !props.lastHackathon ? (
+        <div>Something went wrong...</div>
+      ) : (
+        <div className={classes.inner}>
+          <Text className={classes.smallerText}>
+            Last Hackathon:{' '}
+            <Text component='span' className={classes.highlight} inherit>
+              {props.lastHackathon.title}
+            </Text>
+          </Text>
+
+          {new Date(props.lastHackathon.endDate) < MIN_DATE ? (
+            ''
+          ) : (
+            <Text className={classes.smallerText}>
+              Ended on:{' '}
+              {new Date(props.lastHackathon.endDate).toLocaleDateString(
+                'en-US',
+                {
+                  month: 'short',
+                  day: '2-digit',
+                  year: 'numeric',
+                }
+              )}
+            </Text>
+          )}
+
+          <Text className={classes.smallerText}>
+            Next Hackathon:{' '}
+            <Text component='span' className={classes.highlight} inherit>
+              {props.nextHackathon.title}
+            </Text>
+          </Text>
+
+          {new Date(props.nextHackathon.startDate) > today ? (
+            <Text className={classes.smallerText}>
+              Starts in: <Countdown date={props.nextHackathon.startDate} />
+            </Text>
+          ) : (
+            <Text className={classes.smallerText}>
+              {today < new Date(props.nextHackathon.endDate) &&
+                'Ends in:' + <Countdown date={props.nextHackathon.endDate} />}
+            </Text>
+          )}
+        </div>
+      )}
+
+      <SimpleGrid cols={3} pt={20}>
+        <InfoCard
+          title='Idea Pool'
+          description='Discover the Idea Pool, our space where you can share your ideas.'
+          link='/idea-pool'
+          buttonText='Explore the Idea Pool'
+        />
+
+        <InfoCard
+          title='Hackathons'
+          description='Participate in one of our Hackathons and take your ideas to the next level.'
+          link='/hackathons'
+          buttonText='Join our Hackathons'
+        />
+
+        <InfoCard
+          title='Archive'
+          description='Browse our archive of past Hackathons and ideas.'
+          link='/archive'
+          buttonText='View our Archive'
+        />
+      </SimpleGrid>
     </Container>
   )
 }
