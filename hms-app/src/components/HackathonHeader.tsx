0e6e1afb7ba61f845f1b22dd96fac392da4069f5
katty-bird <82849566+katty-bird@users.noreply.github.com>
2023-06-14 15:26:52 +0200

    #1472 ba: hackathon description visualisation changed (#1513)
    * hackathon description visualization changed
    
    * text centering removed

--- hms-app/src/components/HackathonHeader.tsx
+++ hms-app/src/components/HackathonHeader.tsx
@@ -32,6 +32,11 @@ export default function HackathonHeader(props: IProps) {
               readOnly
               value={hackathonData.description || ''}
               id='hackathonDescriptionEditor'
+              style={{ 
+                color: 'gray', 
+                backgroundColor: 'transparent', 
+                border: 'none'
+              }}
             />
           </Center>
         )}
