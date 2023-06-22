a2a845149b1faf4dddd0f104ecf0a7274cd07a41
katty-bird <ekaterina.kuritsyna@idealo.de>
2023-06-22 15:23:10 +0200

    added bottom chevron and description hiding

--- hms-app/src/components/HackathonHeader.tsx
+++ hms-app/src/components/HackathonHeader.tsx
@@ -1,14 +1,11 @@
-import {
-  Center,
-  Container,
-  Title,
-  useMantineColorScheme,
-} from '@mantine/core'
-import React from 'react'
+
+import { useCallback, useState } from 'react'
+import { Center, Container, Title, useMantineColorScheme, Accordion } from '@mantine/core'
 import { Hackathon } from '../common/types'
 import { MAX_DATE } from '../common/constants'
 import { RichTextEditor } from '@mantine/rte'
 import { heroHeaderStyles } from '../common/styles'
+import { ChevronDown } from 'tabler-icons-react'
 
 type IProps = {
   hackathonData: Hackathon
@@ -20,6 +17,10 @@ export default function HackathonHeader(props: IProps) {
   const isHackathonWithoutDate = () =>
     new Date(hackathonData.startDate) > MAX_DATE
   const { classes } = heroHeaderStyles()
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(true)
+  const handleAccordionChange = useCallback((value: string | null) => {
+    setIsDescriptionOpen(value === 'hackathon-details')
+  }, [])
 
   const formatHackathonDates = () => {
     const startDate = hackathonData.startDate.toLocaleDateString('en-US', {
@@ -36,32 +37,61 @@ export default function HackathonHeader(props: IProps) {
   }
 
   return (
-    <div>
-      <Container className={classes.wrapper} size={1400} style={{ marginBottom: '50px' }}>
-        <Center>
-          <Title className={classes.title} order={2} align={'center'}>
-            {hackathonData.title}
-          </Title>
-        </Center>
+    <Container className={classes.wrapper} size={1400} style={{ marginBottom: '50px' }}>
+      <div>
+        <Title className={classes.title} order={2} align={'center'}>
+          {hackathonData.title}
+        </Title>
         {hackathonData.startDate && hackathonData.endDate && (
-          <Center>
-            <div>
-              {formatHackathonDates()}
-            </div>
-          </Center>
+          <div style={{ textAlign: 'center' }}>
+            {formatHackathonDates()}
+          </div>
         )}
-        {hackathonData.description && (
-          <RichTextEditor
-            readOnly
-            value={hackathonData.description || ''}
-            id='hackathonDescriptionEditor'
-            style={{
-              backgroundColor: 'transparent',
-              border: 'none'
-            }}
-          />
-        )}
-      </Container>
-    </div>
+      </div>
+      <Accordion defaultValue='hackathon-details' 
+        onChange={handleAccordionChange} chevron={null}
+        styles={{
+          item: { border: 'none' }
+        }}
+      >
+        <Accordion.Item value='hackathon-details'>
+          <Accordion.Panel>
+            {isDescriptionOpen && hackathonData.description && (
+              <RichTextEditor
+                readOnly
+                value={hackathonData.description || ''}
+                id='hackathonDescriptionEditor'
+                style={{
+                  backgroundColor: 'transparent',
+                  border: 'none',
+                  // marginBottom: '8px',
+                }}
+              />
+            )}
+          </Accordion.Panel>
+          <Accordion.Control style={{ backgroundColor: 'transparent' }}>
+            <Center>
+              <div>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                  }}
+                >
+                  <ChevronDown
+                    size={24}
+                    style={{
+                      transition: 'transform 0.3s ease-in-out',
+                      transform: isDescriptionOpen ? 'rotate(180deg)' : 'none',
+                    }}
+                  />
+                </div>
+              </div>
+            </Center>
+          </Accordion.Control>
+        </Accordion.Item>
+      </Accordion>
+    </Container>
   )
 }
